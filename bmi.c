d16097895a9a7e452643977b7c81784dee200f49
VegaGabriel18 <vegamartivega8@gmail.com>
2025-06-20 13:49:58 -0300

    Update tp4 bmi.c
    bmi actualizado para tp4

--- bmi.c
+++ bmi.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 
 
-//Vega Gabriel Ezequiel Martin 1R5
+//Repositorio codigo: https://github.com/VegaGabriel18/TP-INFORMATICA-I/tree/main
 int main() {
-        float imc;
-        float peso , altura;
-  
-        printf("Bienvenido a la calculadora de IMC\n");
-        
-        printf("Ingrese su peso en kilogramos: ");
-        scanf("%f", &peso);
+    float peso, altura, imc;
 
-        printf("Ingrese su altura en metros: ");
-        scanf("%f", &altura);
-
-        printf("El peso y la altura ingresados son: %.2f kg y %.2f m\n", peso, altura);
-        
-        printf("\n");
    
-      
-           imc=(peso/(altura*altura));
-           printf("Tu Indice de masa colporal es: %.2f\n",imc);            
-           printf("    \n");
-           printf("INDICE       |   CONDICION\n");
-           printf("--------------------------\n");
-           printf("<18.5            Bajo peso\n");
-           printf("18.5 a 24.9      Normal\n");
-           printf("25.0 - 29.9      Sobrepeso\n");
-           printf(">=30             Obesidad\n");
-    
-           return 0;
-            
+    printf("Ingrese su peso en kilogramos: ");
+    scanf("%f", &peso);
 
+    printf("Ingrese su altura en metros: ");
+    scanf("%f", &altura);
+
+    
+    imc = peso / (altura * altura);
+
+    
+    printf("Su IMC es: %.2f\n", imc);
+
+
+    printf("    \n");
+    printf("INDICE       |   CONDICION\n");
+    printf("--------------------------\n");
+    printf("<18.5            Bajo peso\n");
+    printf("18.5 a 24.9      Normal\n");
+    printf("25.0 - 29.9      Sobrepeso\n");
+    printf(">=30             Obesidad\n");
+    printf("   \n ");
+    
+    if (imc < 18.5) {
+        printf("Usted esta en el rango de: Bajo peso\n");
+    
+    } else if (imc >= 18.5 && imc < 24.9) {
+        printf("Usted esta en el rango de: Peso normal\n");
+        
+    } else if (imc >= 25 && imc < 29.9) {
+        printf("Usted esta en el rango de: Sobrepeso\n");
+    } else {
+        
+        printf("Usted esta en el rango de: Obesidad\n");
+    }
+
+    return 0;
 }
